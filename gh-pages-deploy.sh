f937836086adeafbf1bed9264c37615ec76fe6e8
Oliver Feldmann <oliver.feldmann@sap.com>
2017-12-19 09:09:30 +0100

    Use travis environment variables

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -5,7 +5,7 @@
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     echo "Github page deployment not performed for pull requests"
     # Check for build errors or warnings
-    cd documentation
+    cd $TRAVIS_BUILD_DIR/documentation
     mkdocs build --clean --verbose --strict
     exit $?
 elif [[ "${TRAVIS_BRANCH}" != "master" ]]; then
@@ -14,9 +14,9 @@ elif [[ "${TRAVIS_BRANCH}" != "master" ]]; then
 fi
 
 # We can't deploy if any of these vars are empty
-for variable in "$KEY" "$IV" "$REPO" "$ENCRYPTED_FILE"; do
+for variable in "$KEY" "$IV" "$ENCRYPTED_FILE"; do
     if [ -z "$variable" ]; then
-        echo "ERROR: Found empty KEY, IV, REPO, or ENCRYPTED_FILE variable. Exiting."
+        echo "ERROR: Found empty KEY, IV, or ENCRYPTED_FILE variable. Exiting."
         exit 1
     fi
 done
@@ -27,8 +27,8 @@ chmod 600 deploy-key
 eval `ssh-agent -s`
 ssh-add deploy-key
 git config user.name "Travis CI Publisher"
-git remote add gh-token "git@github.com:$REPO.git";
+git remote add gh-token "git@github.com:$TRAVIS_REPO_SLUG.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages
-echo "Pushing to gh-pages"
-cd documentation
+echo "Pushing to gh-pages of repository $TRAVIS_REPO_SLUG"
+cd $TRAVIS_BUILD_DIR/documentation
 mkdocs gh-deploy -v --clean --remote-name gh-token
