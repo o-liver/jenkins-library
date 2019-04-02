9ced3914bbe23d4adfd7d983f27e85c6595d3560
Oliver Feldmann <oliver.feldmann@sap.com>
2019-04-02 13:19:33 +0200

    Speed up travis build again with cache

--- consumer-test/integrationTestController.groovy
+++ consumer-test/integrationTestController.groovy
@@ -1,2 +1,14 @@
+/*
+In case the build is performed for a pull request TRAVIS_COMMIT is a merge
+commit between the base branch and the PR branch HEAD. That commit is actually built.
+    But for notifying about a build status we need the commit which is currently
+the HEAD of the PR branch.
+
+    In case the build is performed for a simple branch (not associated with a PR)
+In this case there is no merge commit between any base branch and HEAD of a PR branch.
+The commit which we need for notifying about a build status is in this case simply
+TRAVIS_COMMIT itself.
+*/
+
 println "Hello"
 println "commit sha: ${env.TRAVIS_COMMIT}"
