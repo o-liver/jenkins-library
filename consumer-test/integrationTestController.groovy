7ee3ee9abd7a97f7224357f618c600db831246c9
Oliver Feldmann <oliver.feldmann@sap.com>
2019-04-02 13:28:20 +0200

    Use Evlis

--- consumer-test/integrationTestController.groovy
+++ consumer-test/integrationTestController.groovy
@@ -1,14 +1,14 @@
 /*
 In case the build is performed for a pull request TRAVIS_COMMIT is a merge
 commit between the base branch and the PR branch HEAD. That commit is actually built.
-    But for notifying about a build status we need the commit which is currently
+But for notifying about a build status we need the commit which is currently
 the HEAD of the PR branch.
 
-    In case the build is performed for a simple branch (not associated with a PR)
+In case the build is performed for a simple branch (not associated with a PR)
 In this case there is no merge commit between any base branch and HEAD of a PR branch.
 The commit which we need for notifying about a build status is in this case simply
 TRAVIS_COMMIT itself.
 */
+def COMMIT_HASH_FOR_STATUS_NOTIFICATIONS = System.getenv('TRAVIS_PULL_REQUEST_SHA') ?: System.getenv('TRAVIS_COMMIT')
 
-println "Hello"
-println "commit sha: ${System.getenv('TRAVIS_COMMIT')}"
+println "commit sha: ${COMMIT_HASH_FOR_STATUS_NOTIFICATIONS}"
