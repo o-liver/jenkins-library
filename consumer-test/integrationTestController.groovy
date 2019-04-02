d432b0445194239602f1f0fffa6799fb29f26f91
Oliver Feldmann <oliver.feldmann@sap.com>
2019-04-02 15:13:26 +0200

    Try running http-builder

--- consumer-test/integrationTestController.groovy
+++ consumer-test/integrationTestController.groovy
@@ -1,3 +1,7 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
+
+
+
 /*
 In case the build is performed for a pull request TRAVIS_COMMIT is a merge
 commit between the base branch and the PR branch HEAD. That commit is actually built.
@@ -12,3 +16,12 @@ TRAVIS_COMMIT itself.
 def COMMIT_HASH_FOR_STATUS_NOTIFICATIONS = System.getenv('TRAVIS_PULL_REQUEST_SHA') ?: System.getenv('TRAVIS_COMMIT')
 
 println "commit sha: ${COMMIT_HASH_FOR_STATUS_NOTIFICATIONS}"
+
+notifyGithub("pending", "Integration tests in progress.", COMMIT_HASH_FOR_STATUS_NOTIFICATIONS)
+
+def notifyGithub(state, description, hash) {
+    println "[INFO] Notifying about state '${state}' for commit '${hash}'."
+
+    def http = new groovyx.net.http.HTTPBuilder('http://www.codehaus.org')
+    println http
+}
