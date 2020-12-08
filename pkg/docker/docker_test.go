618f123730e0e79e092abd7650672a805295afa9
Oliver Feldmann <oliver.feldmann@sap.com>
2020-12-08 10:42:35 +0100

    DO-NOT-MERGE: Download docker image in test

--- pkg/docker/docker_test.go
+++ pkg/docker/docker_test.go
@@ -1,6 +1,10 @@
 package docker
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,4 +37,27 @@ func TestGetImageSource(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, c.want, got)
 	}
+}
+
+func TestDownloadImageToPath(t *testing.T) {
+	client := Client{}
+
+	options := ClientOptions{ImageName: "maven:3.6.3-jdk-8-slim", RegistryURL: "", LocalPath: ""}
+	client.SetOptions(options)
+
+	imageSource, _ := client.GetImageSource()
+
+	image, err := client.DownloadImageToPath(imageSource, ".")
+	if err != nil {
+		log.Fatalf("failed to download docker image: %v", err)
+	}
+
+	fmt.Println("v1 image:", image)
+
+	fileName := "maven3.tar"
+	tarFilePath := filepath.Join(".", fileName)
+	tarFile, err := os.Create(tarFilePath)
+	err = client.TarImage(tarFile, image)
+
+	assert.NoError(t, err)
 }
